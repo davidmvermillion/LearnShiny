09f95c261849711321fa66d74990b3d3fa342687
David M Vermillion <social@davidmvermillion.com>
2020-12-01 08:45:58 -0500

    Finished Lesson 2
    Created first static page using iterations of HTML tags

--- LearnShiny/App-1/ui2.R
+++ LearnShiny/App-1/ui2.R
@@ -4,9 +4,28 @@ library(shiny)
 ui <- fluidPage(
   titlePanel("My Shiny App"),
   sidebarLayout(
-    sidebarPanel(),
+    sidebarPanel(
+      h2("Installation"),
+      p("Shiny is available on CRAN, so you can install it in the usual way
+        from your R console:",
+        code("install.packages(\"shiny\")")),
+      br(),
+      br(),
+      img(src = "rstudio.png", height = 70, width = 200),
+      p("Shiny is a product of", a(href="https://rstudio.com", "RStudio.")),
+    ),
     mainPanel(
-      img(src = "rstudio.png", height = 140, width = 400)
+      h1("Introducing Shiny"),
+      p("Shiny is a new package from RStudio that makes it", em("incredibly easy"),
+        ("to build interactive web applications with R.")),
+      br(),
+      p("For an introduction and live examples, visit the", a(href="https://shiny.rstudio.com/", "Shiny homepage.")),
+      br(),
+      p(h1("Features")),
+      p("- Build useful web applications with only a few lines of code — no JavaScript required."),
+      p("- Shiny applications are automatically 'live' in the same way that ", strong("spreadsheets"),
+        "are live. outputs change instantly as users modify inputs, without requiring a reload of the browser."),
+      
     )
   )
 )
