552831e2237ade1ad1fd0975405df4b356b1c779
David M Vermillion <social@davidmvermillion.com>
2020-12-17 12:22:23 -0500

    Final Version of First Answer

--- LearnShiny/App-1/cevi.R
+++ LearnShiny/App-1/cevi.R
@@ -5,21 +5,13 @@ ui <- fluidPage(
   titlePanel("censusVis"),
   sidebarLayout(
     sidebarPanel(helpText("Create demographic maps with information from the 2010 US Census"),
-                 h3("Choose a variable to display"),
-                 # selectInput("select", h3("Select box"), 
-                 #             choices = list("Choice 1" = 1, "Choice 2" = 2,
-                 #                            "Choice 3" = 3), selected = 1)),
+                 selectInput("select", h3("Choose a variable to display"),
+                             choices = list("Percent White" = 1, "Percent Black" = 2,
+                                            "Percent Hispanic" = 3, "Percent Asian" = 4), selected = 1),
+                 sliderInput("slider1", h3("Range of interest:"),
+                             min = 0, max = 100, value = c(0, 100)),
                  ),
-    mainPanel(
-      helpText("Create demographic maps with information from the 2010 US Census"),
-      h6("Episode IV", align = "center"),
-      h6("A NEW HOPE", align = "center"),
-      h5("It is a period of civil war.", align = "center"),
-      h4("Rebel spaceships, striking", align = "center"),
-      h3("from a hidden base, have won", align = "center"),
-      h2("their first victory against the", align = "center"),
-      h1("evil Galactic Empire.")
-    )
+    mainPanel()
   )
 )
 
